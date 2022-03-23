6978d55b5ce3fc670c13cca47b3e05ad7e0e2823
João Pedro <jpedro_15@outlook.com>
2022-03-23 16:49:33 -0300

    x

---
+++ Estrutura de dados/estrutura de dados- lista 2/l2e7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Escreva uma função que aloca dinamicamente um vetor inteiro v[0..n-1] e os
+endereços de duas variáveis inteiras, digamos min e max, e deposite nessas
+variáveis o valor de um elemento mínimo e o valor de um elemento máximo
+do vetor. Por fim, realoque o tamanho desse vetor dobrando sua capacidade
+de armazenamento.*/
+
+ int main (){
+
+     return 0;
+ }
---
+++ Estrutura de dados/estrutura de dados- lista 2/l2e8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Escrever um programa que cadastre alguns produtos em um vetor de
+estruturas/registros alocado dinamicamente. Em seguida, imprima uma lista
+com o código e nome da cada produto. Por último, consulte o preço de um
+produto através de seu código*/
+
+int main (){
+
+}
